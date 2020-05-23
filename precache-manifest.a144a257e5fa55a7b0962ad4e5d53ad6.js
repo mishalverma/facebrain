48772dad89dbac012721a0554ea2ab8a6735b888
mishalverma <vermamishal@gmail.com>
2020-05-23 16:34:02 +0530

    Updates

---
+++ precache-manifest.a144a257e5fa55a7b0962ad4e5d53ad6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dc30ca45c53e66be797dd326c9eb73",
+    "url": "/facebrain/index.html"
+  },
+  {
+    "revision": "988bddd71c3e03d6e062",
+    "url": "/facebrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dcdc61d33196186d2c6e",
+    "url": "/facebrain/static/css/main.44755526.chunk.css"
+  },
+  {
+    "revision": "988bddd71c3e03d6e062",
+    "url": "/facebrain/static/js/2.35623477.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facebrain/static/js/2.35623477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdc61d33196186d2c6e",
+    "url": "/facebrain/static/js/main.1f6599eb.chunk.js"
+  },
+  {
+    "revision": "72febec44066423c6655",
+    "url": "/facebrain/static/js/runtime-main.114c3585.js"
+  }
+]);
